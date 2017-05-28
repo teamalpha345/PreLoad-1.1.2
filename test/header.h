91d3e80354e3f4ecf17272696d9bb4e28685191c
teamalpha345 <teamalpha345@gmail.com>
2017-05-28 16:46:06 +1200

    preload

---
+++ test/header.h
@@ -0,0 +1,10 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+    float numStandardsStandard(float price, float numDrinks, float standards);
+    float numStandardsPercent(float price, float numDrinks, float volumne, float percent);
+    float compareResults(float result);
+   #ifdef __cplusplus
+}
+#endif
+
